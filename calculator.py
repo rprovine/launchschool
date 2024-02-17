8ac758c32cdf617fc88c563d2e4bd331d124e45f
Reno Provine <rprovine@gmail.com>
2024-02-16 20:21:06 -1000

    refactor of calculator

--- calculator.py
+++ calculator.py
@@ -3,7 +3,16 @@ Calculator program that takes two numbers
 and an operation and performs the operation
 on the two numbers.
 """
-
+def prompt(message):
+    print(f'=> {message}')
+    
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+    
+    return False
 
 
 # Ask the user for the first number
@@ -12,22 +21,38 @@ on the two numbers.
 # Perform the operation
 # Print the result
 
-print('Welcome to the calculator!')
+prompt('Welcome to the calculator!')
 
-print('Enter the first number:')
+prompt('Enter the first number:')
 number1 = input()
-print('Enter the second number:')
+
+while invalid_number(number1):
+    prompt('Invalid number. Please enter a valid number:')
+    number1 = input()
+    
+prompt('Enter the second number:')
 number2 = input()
-print('What operation would you llike to perform?\n 1) Add 2) Subtract 3) Multiply 4) Divide')
+
+while invalid_number(number2):
+    prompt('Invalid number. Please enter a valid number:')
+    number2 = input()
+    
+prompt('What operation would you llike to perform?\n 1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
 
-if operation == '1':
-    output = int(number1) + int(number2)
-elif operation == '2':
-    output = int(number1) - int(number2)
-elif operation == '3':
-    output = int(number1) * int(number2)
-elif operation == '4':
-    output = int(number1) / int(number2)
+while operation not in ['1', '2', '3', '4']:
+    prompt('Invalid operation. You must choose 1, 2, 3, or 4:')
+    operation = input()
 
-print(f'The result is {output}/n')
+match operation:
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output = int(number1) * int(number2)
+    case '4':
+        output = int(number1) / int(number2)
+    
+
+prompt(f'The result is {output}')
