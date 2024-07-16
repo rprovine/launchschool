029a000c8141c55637dabfe6063239681eaef05d
rprovine <rprovine@gmail.com>
2024-07-16 08:59:01 -1000

    final calculator version

--- calculator.py
+++ calculator.py
@@ -1,59 +1,70 @@
-print('Welcome to my calculator program')
+import json
 
 
-# Add a prompt icon for display purposes
 def prompt(message):
     print(f"==> {message}")
 
 
-# Define an invalid number
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
-
     return False
 
 
-# Ask the user for the first number
-prompt('What is the first number?')
-num1 = input()
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
 
-# Check validity of first number
-while invalid_number(num1):
-    prompt("Hmm... That doesn't look like a valid number")
-    num1 = input()
 
-# Ask the user for the second number
-prompt('What is the second number?')
-num2 = input()
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
 
-# Check validity of second number
-while invalid_number(num2):
-    prompt("Hmm... That doesn't look like a valid number")
-    num2 = input()
+print(messages('welcome'))
 
-# Ask the user for the operation to be performed
-prompt('What operation would you like to be performed?\n1)Add '
-       '2) Subtract 3)Multiply 4) Divide 5')
-operation = input()
+while True:
+    while True:
+        prompt(messages('number_prompt_1'))
+        number1 = input()
 
-# Check validity of operation selection
-while invalid_number(operation):
-    prompt("Hmm... That doesn't look like a valid operation")
-    operation = input()
+        if not invalid_number(number1):
+            break
 
-# Perform the calculation of the two numbers - change to case match
-match operation:
-    case '1':
-        result = int(num1) + int(num2)
-    case '2':
-        result = int(num1) - int(num2)
-    case '3':
-        result = int(num1) * int(num2)
-    case '4':
-        result = int(num1) / int(num2)
+        prompt(messages('invalid_number'))
 
-# Display the result of the calculation
-prompt(f"The answer is {result}!")
+    while True:
+        prompt(messages('number_prompt_2'))
+        number2 = input()
+
+        if not invalid_number(number2):
+            break
+
+        prompt(messages('invalid_number'))
+
+    while True:
+        prompt(messages('operation_prompt'))
+        operation = input()
+
+        if operation in ["1", "2", "3", "4"]:
+            break
+
+        prompt(messages('invalid_operation'))
+
+    match operation:
+        case "1":
+            output = float(number1) + float(number2)
+        case "2":
+            output = float(number1) - float(number2)
+        case "3":
+            output = float(number1) * float(number2)
+        case "4":
+            output = float(number1) / float(number2)
+
+    output = round(output, 2)  # Round the result to two decimals
+
+    prompt(messages('result').format(output=output))
+
+    prompt(messages('another_operation'))
+    answer = input()
+    if answer[0].lower() != 'y':
+        break
